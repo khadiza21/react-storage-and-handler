80f9404d2e1ae8182a788c69056926408cb7da2d
khadiza21 <bibikhadiza474@gmail.com>
2022-03-22 23:35:39 +0600

    import, export default, and export by object

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
+import add from "../../utilities/add";
+import { division, multiply } from "../../utilities/calculate";
 
 const Cosmetics = () => {
-    const first = 55;
-    const second = 66;
-    const total = add(first,second);
-    return (
-        <div>
-            <h1>This is BK</h1>
-            <p>Total: {total}</p>
-        </div>
-    );
+  const first = 55;
+  const second = 12;
+  const summetion = add(first, second);
+  const divide = division(first, second);
+  const multiplication = multiply(first, second);
+  return (
+    <div>
+      <h1>This is BK</h1>
+      <h3>Addition: {summetion}</h3>
+      <h3>Division: {divide}</h3>
+      <h3>Multiplication: {multiplication}</h3>
+    </div>
+  );
 };
 
 export default Cosmetics;
---
+++ src/utilities/calculate.js
@@ -0,0 +1,7 @@
+const division = (num1, num2) => {
+  return num1 / num2;
+};
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+export { division, multiply };
