0e25f7d2d8e2fcdd6cd071fd7e14e09d55e9784b
khadiza21 <bibikhadiza474@gmail.com>
2022-03-23 01:25:19 +0600

    event handler call with parameters

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,14 +1,20 @@
 import React from "react";
-import "./Cosmetic.css"
+import "./Cosmetic.css";
 const Cosmetic = (props) => {
   const { name, price, id } = props.cosmetic;
+  const addToCart = (id) =>{
+console.log(id);
+  }
+  const addToCartWithParam = () => addToCart(id);
   return (
-    <div className="product">
+    <div className="product shadow rounded ">
       <h2>Buy this: {name}</h2>
       <h5>Only for: $ {price}</h5>
-      <p>
+      <p className="fw-bold">
         <small>It has id: {id}</small>
       </p>
+      <button onClick={addToCartWithParam} className="btn btn-outline-warning">Add to Cart</button>
+      <button onClick={ () => addToCart(id)} className="btn btn-outline-success my-3 mx-2">Add to Cart ShortCurt</button>
     </div>
   );
 };
--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import add from "../../utilities/add";
 import { division, multiply } from "../../utilities/calculate";
 import Cosmetic from "../Cosmetic/Cosmetic";
@@ -10,21 +10,28 @@ const Cosmetics = () => {
   const summetion = add(first, second);
   const divide = division(first, second);
   const multiplication = multiply(first, second);
-  const cosmeticsO = [
-    { id: 1, name: "Alta", price: 100 },
-    { id: 2, name: "Alita", price: 200 },
-    { id: 3, name: "Blta", price: 300 },
-    { id: 4, name: "Palta", price: 500 },
-    { id: 5, name: "Calta", price: 400 },
-  ];
+  // const cosmeticsO = [
+  //   { id: 1, name: "Alta", price: 100 },
+  //   { id: 2, name: "Alita", price: 200 },
+  //   { id: 3, name: "Blta", price: 300 },
+  //   { id: 4, name: "Palta", price: 500 },
+  //   { id: 5, name: "Calta", price: 400 },
+  // ];
+
+  const [cosmeticsOP, setCosmetics] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setCosmetics(data));
+  }, []);
   return (
     <div>
       <h1>This is BK</h1>
       <h3>Addition: {summetion}</h3>
       <h3>Division: {divide}</h3>
       <h3>Multiplication: {multiplication}</h3>
-      <div className="products">
-        {cosmeticsO.map((cosmetic) => (
+      <div className="products container my-5">
+        {cosmeticsOP.map((cosmetic) => (
           <Cosmetic key={cosmetic.id} cosmetic={cosmetic}></Cosmetic>
         ))}
       </div>
