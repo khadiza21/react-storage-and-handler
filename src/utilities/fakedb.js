d3544058378ad75d6382c8584f9dafc3bec1c394
khadiza21 <bibikhadiza474@gmail.com>
2022-03-23 02:40:25 +0600

    add to cart numbers and key adding in local storage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,11 +1,34 @@
 // use local storage to manage cart data
 const addToDb = (id) => {
-  const itemCount = localStorage.getItem(id);
-  if (itemCount == null) {
-    localStorage.setItem(id, 1);
+  // this code for only id
+  //   const itemCount = localStorage.getItem(id);
+  //   if (itemCount == null) {
+  //     localStorage.setItem(id, 1);
+  //   } else {
+  //     const totalItem = parseInt(itemCount);
+  //     localStorage.setItem(id, totalItem + 1);
+  //   }
+
+  // this code is for only object
+  let shoppingCart;
+
+  //get the shopping cart
+  const storedCart = localStorage.getItem("shopping-cart");
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
   } else {
-    const totalItem = parseInt(itemCount);
-    localStorage.setItem(id, totalItem + 1);
+    shoppingCart = {};
   }
+
+  //add quantity
+  const quantity = shoppingCart[id];
+  if (quantity) {
+    const newQuantity = quantity + 1;
+    shoppingCart[id] = newQuantity;
+  } else {
+    shoppingCart[id] = 1;
+  }
+
+  localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
 };
 export { addToDb };
