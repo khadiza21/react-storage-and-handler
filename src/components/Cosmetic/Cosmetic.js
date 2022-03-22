e7c95c8170d45095262cef8db341bf3309944744
khadiza21 <bibikhadiza474@gmail.com>
2022-03-23 03:03:48 +0600

    remove cart property or object  in local storage

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { addToDb } from "../../utilities/fakedb";
+import { addToDb, removeFromDb } from "../../utilities/fakedb";
 import "./Cosmetic.css";
 const Cosmetic = (props) => {
   const { name, price, id } = props.cosmetic;
   const addToCart = (id) => {
     // console.log(id);
     // localStorage.setItem(id, 1);
-    addToDb(id)
+    addToDb(id);
+  };
+
+  const removeFromCart = (id) => {
+    removeFromDb(id);
   };
   //const addToCartWithParam = () => addToCart(id);
   return (
@@ -20,9 +24,16 @@ const Cosmetic = (props) => {
       {/* ShortCurt */}
       <button
         onClick={() => addToCart(id)}
-        className="btn btn-outline-warning "
+        className="btn btn-outline-warning fw-bold"
       >
         Add to Cart{" "}
+      </button>
+
+      <button
+        onClick={() => removeFromCart(id)}
+        className="btn btn-outline-danger fw-bold mx-2"
+      >
+        Remove Cart
       </button>
     </div>
   );
